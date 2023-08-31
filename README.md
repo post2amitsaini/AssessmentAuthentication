0b1b97788370091ed7e34499c50f6afab17f8279
Amit Saini <post2amitsaini@gmail.com>
2023-08-31 19:37:11 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AssessmentAuthentication
+
+A description of this package.
---
+++ Sources/AssessmentAuthentication/AssessmentAuthentication.swift
@@ -0,0 +1,6 @@
+public struct AssessmentAuthentication {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
