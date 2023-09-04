4ab5c2bf689378bd7be6600cbe8999d504650552
Amit Saini <post2amitsaini@gmail.com>
2023-09-04 12:51:22 +0530

    SignUp View UI added

---
+++ Sources/AssessmentAuthentication/Presentation/Account/SignUp/View/SignUpView.swift
@@ -0,0 +1,81 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Amit Saini on 04/09/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    public var body: some View {
+        ZStack {
+            Color.black
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.green, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y: -350)
+            
+            VStack(spacing: 20) {
+                Text("SignUp")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .offset(x: -100, y: -100)
+                
+                TextField("Email" , text: $email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty)  {
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                Button {
+                    //Login
+                } label: {
+                    Text("SignUp")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(.linearGradient(colors: [.green, .blue], startPoint: .top, endPoint: .bottomTrailing))
+                        )
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 110)
+                
+                
+            }
+            .frame(width: 350)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
